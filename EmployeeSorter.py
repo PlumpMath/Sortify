c4414fb08b0098088d97d939b4188708131cf1a9
Mike <mikejamesbelanger@gmail.com>
2015-08-19 16:46:17 -0400

    Update EmployeeSorter.py
    Added clause to skip over file-names that it doesn't understand, so it continues processing the rest of the file-list.

--- EmployeeSorter.py
+++ EmployeeSorter.py
@@ -4,9 +4,13 @@ import sys
 import PatternMatch
 import FolderManager
 
-def employee_sorter(source, destination, employeeTxtDir, extension='.pdf', delimiters=[], threshold=2):
+def employee_sorter(employeeTxt, source, destination, extension='.pdf', delimiters=['-', ',',' '], threshold=2):
+
     #First, establish source files
     #Scan a folder of imported pdfs to categorize
+
+    #Load each employee as a pattern from each line of text file.
+    employee_list = PatternMatch.TextRead(employeeTxt, delimiters=delimiters)
 
     #Now examine files with a FolderManager instance.
     src_folder = FolderManager.RootDirectory(source, destination)
@@ -14,62 +18,54 @@ def employee_sorter(source, destination, employeeTxtDir, extension='.pdf', delim
     #Search for all available PDFs
     src_folder.recursive_search(extension, path=None)
 
-    #Create an instance of TextReadDir on all text file names for a directory
-    employee_list = PatternMatch.TextReadDir(employeeTxtDir, delimiters=delimiters)
-
-    #Now loop through to create subdirectories based on all the text files.
-    text_file_list = employee_list.get_text_file_names()
-
-    for text_files in text_file_list:
-        folder = str(text_files)
-        folder_name = os.path.join(destination, folder)
-        src_folder.create_dir(folder_name)
-
-
+    for file in src_folder.files:
         #Create a pattern object('query') based on the filenames in source directroy.
-        for file in src_folder.files:
-            query = str(file.file_name)
-            query = PatternMatch.Pattern(query)
+        query = str(file.file_name)
+        query = PatternMatch.Pattern(query)
 
-            #Breaks up name into meaningful chunks to match with.
-            query.chunkify(delimiters)
+        #Breaks up name into meaningful chunks to match with.
+        query.chunkify(delimiters)
 
-            #Create a variable for a potential match in chunks between file names and
-            #the input text file.
-            name_match = employee_list.lookup_pattern(query, threshold)
+        #Create a variable for a potential match in chunks between file names and
+        #the input text file.
+        name_match = employee_list.lookup(query, threshold)
 
-            #As soon as the variable contains matches, that means we have a hit, and
-            #now begin to move files over to their own directory.
-            if name_match:
-                #Create a new name for the file, based on the convention
-                new_name = name_match
+        #As soon as the variable contains matches, that means we have a hit, and
+        #now begin to move files over to their own directory.
+        if name_match:
+            #Create a new name for the file, based on the convention
+            new_name = name_match
 
-                #Rename file itself to match directory, and number it if there's more
-                #than one.
-                file.new_file_name = new_name
+            #Create new directory for employee, then move its file there.
+            src_folder.create_dir(new_name)
 
-                #Define and create destination path
-                file.destination_path = os.path.join(folder_name, file.new_file_name)
-                os.mkdir(file.destination_path)
+            #Rename file itself to match directory, and number it if there's more
+            #than one.
+            file.new_file_name = new_name
 
-                print("Destination: ", file.destination_path)
-                print("New file name: ", file.new_file_name)
-                file.move()
+            #Finally move the file to the new path.
+            file.destination_path = os.path.join(src_folder.dest_path, file.new_file_name)
+            file.move()
 
-            #If no matching pattern is found, put in an unknown folder.
+        #If no matching pattern is found, put in an unknown folder.
+        else:
+            unknown_folder = (os.path.join(src_folder.dest_path, file.file_name))
+
+            if os.path.exists(unknown_folder):
+                pass
             else:
-                unknown_folder = os.path.join(src_folder.dest_path, file.file_name)
+                os.mkdir(unknown_folder)
 
-                if os.path.exists(unknown_folder):
-                    pass
-                else:
-                    os.mkdir(unknown_folder)
+            file.destination_path = unknown_folder
+            file.new_file_name = file.file_name
 
-                file.destination_path = unknown_folder
-                file.new_file_name = file.file_name
+            #Finally move the file over.
 
-                #Finally move the file over.
+            try:
                 file.move()
+            except FileNotFoundError:
+                print('file: %s could not be found in %s' % (file.new_file_name, file.destination_path))
+                continue
 
 def description():
     '''Just describes procedure of script if invoked improperly.'''
@@ -91,16 +87,16 @@ if __name__ == '__main__':
     elif length == 3:
 
         #Get all relevant arguments
-        txt_file_dir = sys.argv[1]
+        txt_file = sys.argv[1]
         source = os.getcwd()
         destination = os.path.join(source, sys.argv[2])
 
         #check to make sure txt file exists
-        if os.path.exists(txt_file_dir):
-            print("Txt file directory found")
+        if os.path.exists(txt_file):
+            print("Txt file found")
 
-        elif os.path.exists(txt_file_dir) == False:
-            print("\n**Missing text file directory, this won't continue")
+        elif os.path.exists(txt_file) == False:
+            print("\n**Missing text file, this won't continue")
             sys.exit()
 
         try:
@@ -109,7 +105,8 @@ if __name__ == '__main__':
         except:
             pass
 
-        employee_sorter(source, destination, txt_file_dir, delimiters=['-', ',',' '])
+        #Now invoke the actual sorting function
+        employee_sorter(txt_file, source, destination)
 
     else:
         description()
